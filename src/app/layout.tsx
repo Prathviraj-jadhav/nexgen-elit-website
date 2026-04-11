aab70de1de3e1f7940980ab89798a19d7e8d9b0c
Nexgen Elit Developer <nexgenelit@gmail.com>
2026-04-11 20:57:52 +0530

    feat: add Meta Pixel tracking code to root layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -104,10 +104,35 @@ export default function RootLayout({
         {/* llm.txt AI crawler discovery link (llmstxt.org standard) */}
         <link rel="llms-txt" type="text/plain" href="https://nexgenelit.com/llm.txt" />
         <link rel="apple-touch-icon" href="/nexgenelit.png" />
+        {/* Meta Pixel Code */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '1717276075602623');
+              fbq('track', 'PageView');
+            `,
+          }}
+        />
       </head>
       <body
         className={`${montserrat.variable} ${instrumentSerif.variable} ${poppins.variable} antialiased`}
       >
+        <noscript>
+          <img 
+            height="1" 
+            width="1" 
+            style={{ display: 'none' }}
+            src="https://www.facebook.com/tr?id=1717276075602623&ev=PageView&noscript=1"
+          />
+        </noscript>
         <LoadingScreen />
         <CookieConsent />
         <PWAProvider />
